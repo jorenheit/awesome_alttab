bafdfd97e86e812c1bb0845557e8db40a1bdff25
Andrew Phillips <andrew.phillips@ssc-spc.gc.ca>
2014-10-01 15:07:07 -0300

    Properly position the preview wibox on 2nd monitor

--- init.lua
+++ init.lua
@@ -52,8 +52,8 @@ local function preview()
    local h = w * 0.75  -- widget height
    local textboxHeight = 30
 
-   local x = -preview_wbox.border_width
-   local y = (screen[mouse.screen].geometry.height - h - textboxHeight) / 2
+   local x = screen[mouse.screen].geometry.x - preview_wbox.border_width
+   local y = screen[mouse.screen].geometry.y + (screen[mouse.screen].geometry.height - h - textboxHeight) / 2
    preview_wbox:geometry({x = x, y = y, width = W, height = h + textboxHeight})
 
    -- create a list that holds the clients to preview, from left to right
